027be60a8c654cd9736d6ff17881f7ea5c7a0428
tanggyuri <xorb0835@naver.com>
2023-06-24 15:18:41 +0900

    monami

---
+++ js/collapse.js
@@ -0,0 +1,30 @@
+$(function () {
+    $("#collapseOne").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne1").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne2").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne3").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne4").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne5").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne6").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne7").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne8").collapse("hide");
+  });
+  $(function () {
+    $("#collapseOne9").collapse("hide");
+  });
